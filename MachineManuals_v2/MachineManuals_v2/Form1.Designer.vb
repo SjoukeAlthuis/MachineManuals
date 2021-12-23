6acea3f22278e83db7be6eb3425659e80789b185
SjoukeAlthuis <sjoukealthuis@gmail.com>
2021-12-23 13:09:17 +0100

    Add files via upload

---
+++ MachineManuals_v2/MachineManuals_v2/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 45)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(254, 108)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(280, 20)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 34)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(280, 69)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 37)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Files"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(12, 203)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(254, 225)
+        Me.ListBox2.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(279, 267)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 477)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ MachineManuals_v2/MachineManuals_v2/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    Dim pdf As String = ""
+    Dim path As String = ""
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        'Machines()
+        'populate()
+    End Sub
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "MachineManuals_v2"
+        ListBox1.Items.Add("Grob")
+        ListBox1.Items.Add("Mazak VTC")
+        ListBox1.Items.Add("Okuma OSP100")
+        ListBox1.Items.Add("Okuma OSP200")
+        ListBox1.Items.Add("Starrag")
+        ListBox1.Items.Add("Heckert 400D")
+        ListBox1.Items.Add("Heckert 500D")
+        ListBox1.Items.Add("Heckert H65")
+        ListBox1.Items.Add("Mazak Integrex")
+    End Sub
+
+    Sub Machines()
+        Select Case ListBox1.SelectedItem.ToString()
+            Case "Mazak VTC"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\Mazak"
+            Case "Grob"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\Grob"
+            Case "Okuma OSP100"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\OkumaOSP100"
+            Case "Okuma OSP200"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\OkumaOSP200"
+            Case "Mazak Integrex"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\Integrex"
+            Case "Heckert 400D"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\Heckert400D"
+            Case "Heckert H65"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\HeckertH65"
+            Case "Starrag"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\Starrag"
+            Case "Heckert 500D"
+                path = "C:\Users\sjouke.vanalthuis\Documents\MachineManuals\Heckert500D"
+        End Select
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        pdf = ListBox2.SelectedItem.ToString()
+        'MsgBox("You have selected " + ListBox1.SelectedItem.ToString())
+        Process.Start(path & "\" & pdf & ".pdf")
+    End Sub
+
+    Private Sub populate()
+        Try
+            ListBox2.Items.Clear()
+            For Each str As String In My.Computer.FileSystem.GetFiles(path)
+                Dim itemdir() As String
+                itemdir = Split(str, "\")
+                Dim arrayLengthdir As Integer = itemdir.Length - 1
+
+                Dim itemfile() As String
+                itemfile = Split(itemdir(arrayLengthdir), ".pdf")
+
+                ListBox2.Items.Add(itemfile(0))
+
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub ListBox1_Click(sender As Object, e As EventArgs) Handles ListBox1.Click
+        Machines()
+        Populate()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
